9ac1255171acb638a73e531859f1d69d538273f2
AlexandraSpeshilova <165372876+speshilovaAlexandra@users.noreply.github.com>
2026-04-07 23:31:13 +0500

    Update CartView.vueдолларыминусвайб

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -43,7 +43,7 @@
                 </button>
               </div>
               <div class="item-total">
-                ${{ (item.price * item.qty).toFixed(2) }}
+                {{ (item.price * item.qty).toFixed(2) }}₽
               </div>
               <button class="btn-remove" @click="changeQty(item, -item.qty)" title="Удалить">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/></svg>
