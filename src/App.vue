776be602ea0486895298c22dc34e7048423c5bc2
AlexandraSpeshilova <165372876+speshilovaAlexandra@users.noreply.github.com>
2026-06-08 21:40:05 +0500

    Update App.vue
    ыыв

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
             Каталог
           </router-link> -->
 
+          
           <template v-if="isAuthenticated">
             <router-link to="/profile" class="nav-link nav-profile">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
