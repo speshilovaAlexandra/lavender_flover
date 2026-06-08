6a611112fccac286f55a18d592b9cf5426c871b4
AlexandraSpeshilova <165372876+speshilovaAlexandra@users.noreply.github.com>
2026-06-08 21:45:23 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@
           <!-- <router-link to="/catalog" class="nav-link" active-class="active">
             Каталог
           </router-link> -->
-
-          
           <template v-if="isAuthenticated">
             <router-link to="/profile" class="nav-link nav-profile">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
